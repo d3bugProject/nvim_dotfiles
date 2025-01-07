e486faffb80e87d2de14c01e35cedb91c1a2b2dd
mohamed laalaoui <waterseven.dz@gmail.com>
2025-01-07 22:10:45 +0100

    test

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+
+
+
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
